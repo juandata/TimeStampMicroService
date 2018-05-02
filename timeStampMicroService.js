0fb09a826ab9a87d1ee8ae35ce1d217f891db4e8
Glitch (cyber-clock) <none>
2018-05-02 16:44:43 +0000

    🍯🍔 Checkpoint ./timeStampMicroService.js:1033623/1770

--- timeStampMicroService.js
+++ timeStampMicroService.js
@@ -6,14 +6,19 @@
    var path = require('path')
    var express = require('express');
    var app = express();
-   app.use(express.static('/index.html'||path.join(__dirname, 'views')));
-   app.listen(process.env.PORT);
-   
-/*
-  var express = require('express');
-  var app = express();
+   var fs = require('fs');
+  
+
+  /* //output hello world
    app.get('/', function(req, res) {
      res.end('Hello World!');
    });
    app.listen(process.env.PORT);
    */
+ //process the query and outputs it to the res  
+app.get('/search', function(req, res){
+  var query = req.query;
+  res.send(query);
+});
+app.listen(process.env.PORT);
+  
