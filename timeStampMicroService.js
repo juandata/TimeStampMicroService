21f37fe09213fde7a9a3fde2cd0282cf3c047911
Glitch (cyber-clock) <none>
2018-05-02 19:30:41 +0000

    🔥😬 Checkpoint ./timeStampMicroService.js:1033623/979 ./package.json:1033623/54

--- timeStampMicroService.js
+++ timeStampMicroService.js
@@ -22,14 +22,11 @@ app.get('/search', function(req, res){
 });
 app.listen(process.env.PORT);
   */
+
 app.get('/', function(req, res) {
-  console.log("middleware function called");
-  res.sendFile('index.html', { root: __dirname + '/views'}, function(err){
-    if(err) throw err;
-    console.log("no error");
-  })
+  console.log(res);
+  res.sendFile('index.html', { root: __dirname + '/views'})
 })
 app.listen(process.env.PORT, function () {
   console.log('Node.js listening ...');
 });
-
