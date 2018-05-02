fb444ad5d97b9e3c8bea4b6872235a216e738f97
Glitch (cyber-clock) <none>
2018-05-02 16:34:35 +0000

    🐮🍂 Checkpoint ./views/timeStampApi.html:1033623/776 ./package.json:1033623/58 ./timeStampMicroService.js:1033623/257

--- timeStampMicroService.js
+++ timeStampMicroService.js
@@ -8,3 +8,12 @@
    var app = express();
    app.use(express.static('/index.html'||path.join(__dirname, 'views')));
    app.listen(process.env.PORT);
+   
+/*
+  var express = require('express');
+  var app = express();
+   app.get('/', function(req, res) {
+     res.end('Hello World!');
+   });
+   app.listen(process.env.PORT);
+   */
