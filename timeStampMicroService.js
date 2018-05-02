97753d56cf197a5645d4b7a083a0dced05dc669f
Glitch (cyber-clock) <none>
2018-05-02 16:23:46 +0000

    💃🛥 Checkpoint ./timeStampMicroService.js:1033623/1029 ./package.json:1033623/118

--- timeStampMicroService.js
+++ timeStampMicroService.js
@@ -1,6 +1,10 @@
-var express = require('express');
-  var app = express();
-   app.get('/home', function(req, res) {
-     res.end('Hello World!');
-   });
-   app.listen(process.argv[2]);
+ /******************************************************
+ * This is the timestamp server file
+ * Developed by JDA to potential clients
+ * ***************************************************/
+
+   var path = require('path')
+   var express = require('express');
+   var app = express();
+   app.use(express.static('/index.html'||path.join(__dirname, 'views')));
+   app.listen(process.env.PORT);
