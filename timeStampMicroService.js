cd76152f9cc20cda76888ed126f9251ac6146cd5
Glitch (cyber-clock) <none>
2018-05-03 02:23:03 +0000

    🏇⛴ Checkpoint ./timeStampMicroService.js:1033623/476

--- timeStampMicroService.js
+++ timeStampMicroService.js
@@ -26,18 +26,19 @@ app.listen(process.env.PORT);
 //app.use(express.static('public'));
 app.use(function (req, res, next) {
   //console.log("Middleware de nivel de aplicación sin ninguna vía de acceso de montaje"); 
-  var miliseconds = 1450137600;
-  var unixDate = new Date(miliseconds);
-  console.log(unixDate);
+    var date = req.url.substr(1);
+    var miliseconds = parseInt(date);
+    var unixDate = new Date(miliseconds);
+    console.log(unixDate);
   
-  
+  /*
   console.log('Request URL:', req.url);
   var regExpr = /%/;
   var date = req.url.split(regExpr);
   console.log('divided : ', date);
   var month = date[0].substr(1);
   console.log(month);
-  
+  */
   
   next();
 },function(req, res, next){
